7b958e62742eef70d13ff74b3d0567a47a6f3c0e
abdulhakim_altunkaya <altunkaya2@gmail.com>
2023-02-08 22:03:00 +0300

    form and code is finished ready for first deploy to netlify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
 
 
 function App() {
+  let[inputValue, setInputValue] = useState("");
+  let[displayValue, setDisplayValue] = useState("");
+
+  const api = process.env.REACT_APP_CHATGPT_API;
+  const configuration = new Configuration({apiKey: api});
+  const openai = new OpenAIApi(configuration);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await openai.createCompletion({
+      prompt: inputValue,
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 1000
+    });
+    const message = response.data.choices[0].text;
+    setDisplayValue(message);
+  }
+
   return (
     <div className="App">
-      <p>text text</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <button type="submit">SUBMIT SIR</button>
+      </form>
+      <p>{displayValue}</p>
     </div>
   );
 }
